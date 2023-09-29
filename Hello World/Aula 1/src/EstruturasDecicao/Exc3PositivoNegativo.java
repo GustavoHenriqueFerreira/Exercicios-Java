753bf85e9406c9c71d70f6b3c338b24447bc0063
GustavoHenriqueFerreira <gustavohferreiraalves@gmail.com>
2023-09-28 21:50:23 -0300

    feat: Exercicio Hello World

---
+++ Hello World/Aula 1/src/EstruturasDecicao/Exc3PositivoNegativo.java
@@ -0,0 +1,27 @@
+package EstruturasDecicao;
+
+import java.text.MessageFormat;
+import java.util.Scanner;
+
+public class Exc3PositivoNegativo {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in); //entrada de dados
+
+        System.out.print("Digite um número: "); //saída de dados
+
+        double numero = leitor.nextDouble();
+
+        if(numero > 0){
+            /*console.log(`O ${numero} é positivo`);*/
+            String msg = MessageFormat.format("O {0} é {1}", numero, "positivo");
+            System.out.printf(msg);
+        } else if (numero == 0) {
+            String msg = MessageFormat.format("O {0} é {1}", numero, "neutro");
+            System.out.printf(msg);
+        }
+        else {
+            String msg = MessageFormat.format("O {0} é {1}", numero, "negativo");
+            System.out.printf(msg);
+        }
+    }
+}
---
+++ Hello World/Aula 1/src/EstruturasDecicao/Exc5NumerosIguais.java
@@ -0,0 +1,25 @@
+package EstruturasDecicao;
+
+import java.text.MessageFormat;
+import java.util.Scanner;
+
+public class Exc5NumerosIguais {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in); //entrada de dados
+
+        System.out.print("Digite um número: "); //saída de dados
+        double numero = Double.parseDouble(leitor.nextLine());
+
+        System.out.print("Digite outro número: "); //saída de dados
+        double segundoNumero = Double.parseDouble(leitor.nextLine());
+
+        if(segundoNumero == numero){
+            /*console.log(`O ${numero} é positivo`);*/
+            //String msg = MessageFormat.format("O {0} é {1}", numero, "positivo");
+            System.out.print("Iguais");
+        } else {
+            //String msg = MessageFormat.format("O {0} é {1}", numero, "neutro");
+            System.out.print("Diferentes");
+        }
+    }
+}
